d2ae3c9362725c4ae84a305afc17a478805c5b3c
Ubuntu <ubuntu@ip-172-31-17-113.ec2.internal>
2020-05-26 12:30:14 +0000

    Update

---
+++ copy_logs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ -d $HOME/jarvis-nifi ];then
+	sudo chown ubuntu:ubuntu $HOME/jarvis-nifi/logs/*
+	cp $HOME/jarvis-nifi/logs/nifi-app.log ./
+fi
+
+if [ -d $HOME/minifi ];then
+	sudo chown ubuntu:ubuntu $HOME/minifi/minifi-0.5.0/logs/*
+	cp $HOME/minifi/minifi-0.5.0/logs/minifi-app.log ./
+fi
+
+git add .
+git commit -m "Update"
+git push
